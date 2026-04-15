0880457ba0be741b10133f9f9bf892639ebe4c61
MariamAlhaj <s-mariam.mahmoud@zewailcity.edu.eg>
2026-04-15 13:51:51 +0200

    Add bad code to fail linter

--- bad_code.py
+++ bad_code.py
@@ -1,10 +1,6 @@
-import os, sys 
- 
-def bad_function( ): 
-    x=1 
-    y=2 
-    z=x+y 
-    return z 
- 
-bad_function() 
-print("This will cause linter warnings") 
+def bad_function()
+    x = 1
+    y = 2
+    return x + y
+
+bad_function()
